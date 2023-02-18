dca4582072419a6acc0afe9701427faa2568938f
Dmitrii Pyzhov <d.pyzhov@andersenlab.com>
2023-02-18 19:40:55 +0400

    Initial Commit

---
+++ Inkery/InkeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkeryApp.swift
+//  Inkery
+//
+//  Created by Dmitry Pyzhov on 18.02.23.
+//
+
+import SwiftUI
+
+@main
+struct InkeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
